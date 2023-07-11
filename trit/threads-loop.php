766593f00f84a69eff4e16121b91ca3f3a126422
0xlAp404# <afdipratama07@gmail.com>
2023-07-11 00:06:06 -0700

    Update threads-loop.php

--- trit/threads-loop.php
+++ trit/threads-loop.php
@@ -19,56 +19,31 @@ function request($url, $data = null, $headers = null, $outputheader = null)
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     }
 
-    // curl_setopt($ch, CURLOPT_ENCODING, "GZIP");
     return curl_exec($ch);
 }
 
-$uuid = sprintf(
-    '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-    mt_rand(0, 0xffff),
-    mt_rand(0, 0xffff),
-    mt_rand(0, 0xffff),
-    mt_rand(0, 0x0fff) | 0x4000,
-    mt_rand(0, 0x3fff) | 0x8000,
-    mt_rand(0, 0xffff),
-    mt_rand(0, 0xffff),
-    mt_rand(0, 0xffff)
-);
+function requestToken($username, $password, $uuid)
+{
+    $headers = array();
+    $headers[] = 'X-Bloks-Version-Id: 5f56efad68e1edec7801f630b5c122704ec5378adbee6609a448f105f34a9c73';
+    $headers[] = 'X-Ig-Www-Claim: hmac.AR2jr3_r-N6PqPM09G7tetqnPfD9P_Ux_HFjJvwyPwksRLqR';
+    $headers[] = 'X-Ig-Device-Id: ' . $uuid;
+    $headers[] = 'X-Ig-Android-Id: android-6be35fa278d92525';
+    $headers[] = 'User-Agent: Barcelona 289.0.0.77.109 Android (31/12; 440dpi; 1080x2148; Google/google; sdk_gphone64_arm64; emulator64_arm64; ranchu; en_US; 489720145)';
+    $headers[] = 'Accept-Language: en-US';
 
-$akunIG = [
-    ['username' => 'MASUKAN USERNAME DISINI', 'password' => 'MASUKAN PASSWORD MU DISINI']]; // TIDAK ADA LOGGER
-
-$headers = array();
-$headers[] = 'X-Bloks-Version-Id: 5f56efad68e1edec7801f630b5c122704ec5378adbee6609a448f105f34a9c73';
-$headers[] = 'X-Ig-Www-Claim: hmac.AR2jr3_r-N6PqPM09G7tetqnPfD9P_Ux_HFjJvwyPwksRLqR';
-$headers[] = 'X-Ig-Device-Id: '.$uuid;
-$headers[] = 'X-Ig-Android-Id: android-6be35fa278d92525';
-$headers[] = 'User-Agent: Barcelona 289.0.0.77.109 Android (31/12; 440dpi; 1080x2148; Google/google; sdk_gphone64_arm64; emulator64_arm64; ranchu; en_US; 489720145)';
-$headers[] = 'Accept-Language: en-US';
-$headers[] = 'Host: i.instagram.com';
-
-
-$asciiArt = file_get_contents('ascii.txt');
-echo $asciiArt;
-echo "\n\n";
-$index = 0; 
-while(true) {
-    $username = $akunIG[$index]['username'];
-    $password = $akunIG[$index]['password'];
-    $getToken = request('https://i.instagram.com/api/v1/bloks/apps/com.bloks.www.bloks.caa.login.async.send_login_request/', 'params=%7B%22client_input_params%22%3A%7B%22device_id%22%3A%22android-6be35fa278d92525%22%2C%22login_attempt_count%22%3A1%2C%22secure_family_device_id%22%3A%22%22%2C%22machine_id%22%3A%22ZKoroAABAAGyzN_tN5j5gN3Q0kpR%22%2C%22accounts_list%22%3A%5B%5D%2C%22auth_secure_device_id%22%3A%22%22%2C%22password%22%3A%22'.$password.'%22%2C%22family_device_id%22%3A%22621af360-a821-4229-9e3a-678d59eb7d37%22%2C%22fb_ig_device_id%22%3A%5B%5D%2C%22device_emails%22%3A%5B%5D%2C%22try_num%22%3A1%2C%22event_flow%22%3A%22login_manual%22%2C%22event_step%22%3A%22home_page%22%2C%22openid_tokens%22%3A%7B%7D%2C%22client_known_key_hash%22%3A%22%22%2C%22contact_point%22%3A%22'.$username.'%22%2C%22encrypted_msisdn%22%3A%22%22%7D%2C%22server_params%22%3A%7B%22username_text_input_id%22%3A%22wktbih%3A48%22%2C%22device_id%22%3A%22android-6be35fa278d92525%22%2C%22should_trigger_override_login_success_action%22%3A0%2C%22server_login_source%22%3A%22login%22%2C%22waterfall_id%22%3A%226b2356be-c2a1-41ac-8f81-9af5ec0aee87%22%2C%22login_source%22%3A%22Login%22%2C%22INTERNAL__latency_qpl_instance_id%22%3A196987789700164%2C%22is_platform_login%22%3A0%2C%22credential_type%22%3A%22password%22%2C%22family_device_id%22%3A%22621af360-a821-4229-9e3a-678d59eb7d37%22%2C%22INTERNAL__latency_qpl_marker_id%22%3A36707139%2C%22offline_experiment_group%22%3A%22caa_iteration_v3_perf_ig_4%22%2C%22INTERNAL_INFRA_THEME%22%3A%22harm_f%22%2C%22password_text_input_id%22%3A%22wktbih%3A49%22%2C%22ar_event_source%22%3A%22login_home_page%22%7D%7D&bk_client_context=%7B%22bloks_version%22%3A%225f56efad68e1edec7801f630b5c122704ec5378adbee6609a448f105f34a9c73%22%2C%22styles_id%22%3A%22instagram%22%7D&bloks_versioning_id=5f56efad68e1edec7801f630b5c122704ec5378adbee6609a448f105f34a9c73', $headers, '');
+    $getToken = request('https://i.instagram.com/api/v1/bloks/apps/com.bloks.www.bloks.caa.login.async.send_login_request/', 'params=%7B%22client_input_params%22%3A%7B%22device_id%22%3A%22android-6be35fa278d92525%22%2C%22login_attempt_count%22%3A1%2C%22secure_family_device_id%22%3A%22%22%2C%22machine_id%22%3A%22ZKoroAABAAGyzN_tN5j5gN3Q0kpR%22%2C%22accounts_list%22%3A%5B%5D%2C%22auth_secure_device_id%22%3A%22%22%2C%22password%22%3A%22' . $password . '%22%2C%22family_device_id%22%3A%22621af360-a821-4229-9e3a-678d59eb7d37%22%2C%22fb_ig_device_id%22%3A%5B%5D%2C%22device_emails%22%3A%5B%5D%2C%22try_num%22%3A1%2C%22event_flow%22%3A%22login_manual%22%2C%22event_step%22%3A%22home_page%22%2C%22openid_tokens%22%3A%7B%7D%2C%22client_known_key_hash%22%3A%22%22%2C%22contact_point%22%3A%22' . $username . '%22%2C%22encrypted_msisdn%22%3A%22%22%7D%2C%22server_params%22%3A%7B%22username_text_input_id%22%3A%22wktbih%3A48%22%2C%22device_id%22%3A%22android-6be35fa278d92525%22%2C%22should_trigger_override_login_success_action%22%3A0%2C%22server_login_source%22%3A%22login%22%2C%22waterfall_id%22%3A%226b2356be-c2a1-41ac-8f81-9af5ec0aee87%22%2C%22login_source%22%3A%22Login%22%2C%22INTERNAL__latency_qpl_instance_id%22%3A196987789700164%2C%22is_platform_login%22%3A0%2C%22credential_type%22%3A%22password%22%2C%22family_device_id%22%3A%22621af360-a821-4229-9e3a-678d59eb7d37%22%2C%22INTERNAL__latency_qpl_marker_id%22%3A36707139%2C%22offline_experiment_group%22%3A%22caa_iteration_v3_perf_ig_4%22%2C%22INTERNAL_INFRA_THEME%22%3A%22harm_f%22%2C%22password_text_input_id%22%3A%22wktbih%3A49%22%2C%22ar_event_source%22%3A%22login_home_page%22%7D%7D&bk_client_context=%7B%22bloks_version%22%3A%225f56efad68e1edec7801f630b5c122704ec5378adbee6609a448f105f34a9c73%22%2C%22styles_id%22%3A%22instagram%22%7D&bloks_versioning_id=5f56efad68e1edec7801f630b5c122704ec5378adbee6609a448f105f34a9c73', $headers, '');
     $re = '/Bearer (.*?)\\\\/'; //biar ga error
     preg_match_all($re, $getToken, $header);
 
-    if (!isset($header[1][0])){
+    if (!isset($header[1][0])) {
         echo 'GAGAL GET AUTH, ULANGI..';
-        sleep(10); // Tidur selama 10 detik sebelum mencoba lagi
+        sleep(10);
+        return false;
     } else {
         $token = $header[1][0];
         echo 'Token Kamu : ' . $token;
-
-        likeTimeline($token, $uuid);
-
-        sleep(180); // Tidur selama 180 detik atau 3 menit
+        return $token;
     }
 }
 
@@ -81,19 +56,26 @@ function likeTimeline($token, $uuid)
     $headers[] = 'X-Ig-Android-Id: android-6be35fa278d92525';
     $headers[] = 'User-Agent: Barcelona 289.0.0.77.109 Android (31/12; 440dpi; 1080x2148; Google/google; sdk_gphone64_arm64; emulator64_arm64; ranchu; en_US; 489720145)';
     $headers[] = 'Accept-Language: en-US';
-    $headers[] = 'Authorization: Bearer ' . $token;
-    $headers[] = 'Host: i.instagram.com';
 
-    $likeCount = 0; 
-    $maxLikeCount = 50; 
+    $likeCount = 0;
+    $maxLikeCount = 50;
 
     while ($likeCount < $maxLikeCount) {
+        $headers[] = 'Authorization: Bearer ' . $token; // Tambahkan header authorization dengan token
+
         $getFeeds = request('https://i.instagram.com/api/v1/feed/text_post_app_timeline/', 'feed_view_info=[]&max_id=&pagination_source=text_post_feed_threads&is_pull_to_refresh=0&_uuid=' . $uuid . '&bloks_versioning_id=5f56efad68e1edec7801f630b5c122704ec5378adbee6609a448f105f34a9c73', $headers, '');
         $parsegetFeeds = json_decode($getFeeds, TRUE);
 
         if (isset($parsegetFeeds['message']) && $parsegetFeeds['message'] == "login_required") {
-            echo "TOKEN KAMU EXPIRED SILAHKAN JALANKAN ULANG PROGRAM";
-            exit();
+            echo "TOKEN KAMU EXPIRED, MEMINTA TOKEN BARU...\n";
+            $token = requestToken();
+            if (!$token) {
+                echo 'GAGAL GET AUTH, ULANGI..';
+                sleep(10);
+                continue;
+            }
+            $headers[6] = 'Authorization: Bearer ' . $token; // Update header authorization dengan token baru
+            continue; // Lanjutkan ke iterasi berikutnya
         }
 
         if (isset($parsegetFeeds['items']) && is_array($parsegetFeeds['items'])) {
@@ -113,9 +95,9 @@ function likeTimeline($token, $uuid)
                         echo "Gagal Melakukan Like" . PHP_EOL;
                     }
                 }
-                
+
                 if ($likeCount >= $maxLikeCount) {
-                    break 2; 
+                    break 2;
                 }
             }
         }
@@ -123,4 +105,62 @@ function likeTimeline($token, $uuid)
 }
 
 
+$uuid = sprintf(
+    '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+    mt_rand(0, 0xffff),
+    mt_rand(0, 0xffff),
+    mt_rand(0, 0xffff),
+    mt_rand(0, 0x0fff) | 0x4000,
+    mt_rand(0, 0x3fff) | 0x8000,
+    mt_rand(0, 0xffff),
+    mt_rand(0, 0xffff),
+    mt_rand(0, 0xffff)
+);
 
+$akunIG = [
+    ['username' => 'uncrownedw', 'password' => 'zahranabila5']
+];
+
+$headers = array();
+$headers[] = 'X-Bloks-Version-Id: 5f56efad68e1edec7801f630b5c122704ec5378adbee6609a448f105f34a9c73';
+$headers[] = 'X-Ig-Www-Claim: hmac.AR2jr3_r-N6PqPM09G7tetqnPfD9P_Ux_HFjJvwyPwksRLqR';
+$headers[] = 'X-Ig-Device-Id: ' . $uuid;
+$headers[] = 'X-Ig-Android-Id: android-6be35fa278d92525';
+$headers[] = 'User-Agent: Barcelona 289.0.0.77.109 Android (31/12; 440dpi; 1080x2148; Google/google; sdk_gphone64_arm64; emulator64_arm64; ranchu; en_US; 489720145)';
+$headers[] = 'Accept-Language: en-US';
+$headers[] = 'Host: i.instagram.com';
+
+$asciiArt = file_get_contents('ascii.txt');
+echo $asciiArt;
+echo "\n\n";
+
+$index = 0;
+$akunCount = count($akunIG);
+
+while (true) {
+    $username = $akunIG[$index]['username'];
+    $password = $akunIG[$index]['password'];
+
+    $token = requestToken($username, $password, $uuid);
+
+    if (!$token) {
+        echo 'GAGAL GET AUTH, ULANGI..';
+        sleep(10);
+        continue;
+    }
+
+    echo 'Token Kamu : ' . $token;
+
+    try {
+        likeTimeline($token, $uuid);
+        sleep(180);
+    } catch (Exception $e) {
+        echo $e->getMessage() . ", Mengambil Token Baru...\n";
+        continue;
+    }
+
+    $index++;
+    if ($index >= $akunCount) {
+        $index = 0;
+    }
+}
